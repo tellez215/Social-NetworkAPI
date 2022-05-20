f53b440c1c9c9f2af08590ac98a450d12f18604a
CT <tellezc007@gmail.com>
2022-05-20 15:02:02 -0700

    add UserSchema and export user

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        Unique: true,
+        Required: true,
+        trimmed: true
+    },
+
+    email: {
+        type: String,
+        Required: true,
+        Unique: true,
+        Match: [/.+@.+\..+/, 'Enter a valid e-mail address']
+    },
+    thoughts: [],
+    friends: [this]
+
+
+}, {
+    toJSON: {
+        getters: true,
+        virtuals: true
+    }
+});
+
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
